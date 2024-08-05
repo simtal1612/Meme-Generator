f86a12350f48fdfe6fe2ecfbb3d9bccb92ecbc51
simtal1612 <simtal785@gmail.com>
2024-08-05 21:09:20 +0300

    MondaySubmission

--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
-var gElCanvas 
-var gCtx 
-var gCurrText = '' 
-var gCurrColor = '#000000' 
-var gCurrImage = null 
+var gElCanvas
+var gCtx
+var gCurrText = ''
+var gCurrColor = '#000000'
+var gCurrImage = null
 
 function onInit() {
-    console.log('onInit called') 
-    gElCanvas = document.querySelector('.canvas')  
+    console.log('onInit called')
+    gElCanvas = document.querySelector('.canvas')
     if (!gElCanvas) {
-        console.error('Canvas element not found') 
-        return 
+        console.error('Canvas element not found')
+        return
     }
-    gCtx = gElCanvas.getContext('2d') 
+    gCtx = gElCanvas.getContext('2d')
     if (!gCtx) {
-        console.error('Failed to get canvas context') 
-        return 
+        console.error('Failed to get canvas context')
+        return
     }
-    console.log('gCtx initialized:', gCtx) 
-    renderGallery() 
-    renderMeme() 
-    window.addEventListener('resize', resizeCanvas) 
+    console.log('gCtx initialized:', gCtx)
+    renderGallery()
+    renderMeme()
+    window.addEventListener('resize', resizeCanvas)
+    gElCanvas.addEventListener('click', handleCanvasClick)
 }
 
-document.addEventListener('DOMContentLoaded', onInit) 
+document.addEventListener('DOMContentLoaded', onInit)
 
 function resizeCanvas() {
-    console.log('resizeCanvas called') 
-    const elContainer = document.querySelector('.canvas-container') 
-    gElCanvas.width = elContainer.clientWidth - 40 
-    gElCanvas.height = elContainer.clientHeight - 40 
-    renderMeme() 
+    console.log('resizeCanvas called')
+    const elContainer = document.querySelector('.canvas-container')
+    gElCanvas.width = elContainer.clientWidth - 40
+    gElCanvas.height = elContainer.clientHeight - 40
+    renderMeme()
 }
-// comon commit
